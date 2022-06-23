4dddcf6fb9f1d0add1b8c731d9b5e2b0a21f7a7f
Andreas Ink <andreasink17@gmail.com>
2022-06-22 21:18:19 -0400

    First commit

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RNSamsungHealth from 'rn-samsung-health'
 
 export default function App() {
+  useEffect(()=>{
+    health();
+  },[])
+  const health = async() => {
+    try{
+      const auth = await RNSamsungHealth.authorize();
+      let startDate = new Date().setDate(new Date().getDate()-30); // 30 days back date
+      let endDate = new Date().getTime(); //today's date
+      let opt = {startDate, endDate};
+      const steps = await RNSamsungHealth.getDailyStepCount(opt);
+
+    }catch(error){
+      console.log("error ", error)
+    }
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
