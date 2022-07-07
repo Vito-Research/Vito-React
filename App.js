f590e5dd22e93fbdcd2c8b0858ed8564a0499627
jameslatimerr <james.latimer@ucf.edu>
2022-07-06 23:36:59 -0400

    Imported home in App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 import styles from './styles'
 import { styleProps } from 'react-native-web/dist/cjs/modules/forwardedProps';
 import BChart from './components/BarChart.js'
-
+import Home from './components/Home.js'
 export default function App() {
  
 
